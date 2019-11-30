9d090c4e9a8be531a349064c15b3f798c46944e6
Yue Pan <ypan13@binghamton.edu>
2019-11-30 12:19:28 -0500

    round loop

--- Client.java
+++ Client.java
@@ -1,31 +1,52 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String [] args){
-        if (args.length != 3) {
-            System.out.println("Usage: java Client <ServerIP> <ServerPort> <Choice(lowercase)>");
+        /* args check */
+        if (args.length != 2) {
+            System.out.println("Usage: java Client <ServerIP> <ServerPort>");
             System.exit(-1);
         }
         try {
-            int port = Integer.valueOf(args[1]);
-            Socket clientSocket = new Socket(args[0],port);
-            /*
-            switch (args[2]){
-                case "rock": output = 1;
-                case "paper": output = 2;
-                case "scissor": output = 3;
-            }
-            */
-            /* send choice */
+            /* TCP connection */
+            Socket clientSocket = new Socket(args[0],Integer.valueOf(args[1]));
             PrintWriter outWriter = new PrintWriter(clientSocket.getOutputStream());
-            outWriter.println(args[2]);
-            outWriter.flush();
-            /* receive result */
             BufferedReader in_buf = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("Result: " + in_buf.readLine());
+            /* game rounds loop*/
+            while(true){
+                /* keyboard input choice */
+                Scanner scan = new Scanner(System.in);
+                String input;
+                System.out.println("Input Your Choice: paper, rock or scissor");
+                if (scan.hasNextLine()) {
+                    input = scan.nextLine();
+                }else{
+                    System.out.println("key board input error");
+                    break;
+                }
+                /* send choice to server*/
+                outWriter.println(input);
+                outWriter.flush();
+                /* receive result from server*/
+                System.out.println("Result: " + in_buf.readLine());
+                /* end game or start next round*/
+                System.out.println("Ready for Next Round?");
+                System.out.println("press any key to continue or input \"end\" to end this game");
+                if (scan.hasNextLine()){
+                    outWriter.println(scan.nextLine());
+                    outWriter.flush();
+                    if (in_buf.readLine().equals("end")){
+                        break;
+                    }else {
+                        System.out.println("Next Round");
+                    }
+                }
+            }
             in_buf.close();
             outWriter.close();
+            System.out.println("End Game");
         } catch (Exception x) {
             x.printStackTrace();
         }
--- Server.java
+++ Server.java
@@ -9,22 +9,26 @@ public class Server {
             System.exit(-1);
         }
         int port = Integer.valueOf(args[0]);
-        /* listening TCP */
         try {
+            /* server listening TCP */
             ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("***Server start");
             System.out.println("IP: "+ InetAddress.getLocalHost().getHostAddress() + ", Port: " + port);
+            /* TCP connection */
+            Socket socket1 = serverSocket.accept();
+            Socket socket2 = serverSocket.accept();
+            BufferedReader buffer1 = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
+            BufferedReader buffer2 = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
+            PrintWriter Writer1 = new PrintWriter(socket1.getOutputStream());
+            PrintWriter Writer2 = new PrintWriter(socket2.getOutputStream());
+            /* game rounds loop */
+            System.out.println("***Two player connected");
             while(true){
+                System.out.println("**Round Start");
                 String ret1;    //return value to client 1
                 String ret2;    // return value to client 2
-                System.out.println("***New Game");
-                /* TCP connection */
-                Socket socket1 = serverSocket.accept();
-                Socket socket2 = serverSocket.accept();
-                /* read client 1 */
-                BufferedReader buffer1 = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
+                /* read choice from client*/
                 String choice1 = buffer1.readLine();
-                /* read client 2 */
-                BufferedReader buffer2 = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
                 String choice2 = buffer2.readLine();
                 /* check invalid input choice */
                 if((! choice1.equals("rock")) && (! choice1.equals("paper")) && (! choice1.equals("scissor"))){
@@ -50,21 +54,33 @@ public class Server {
                         ret1 = "You Win";
                         ret2 = "You Loss";
                     }
-                    //((choice2.equals("rock")&&choice1.equals("paper")) || (choice2.equals("paper")&&choice1.equals("scissor")) || (choice2.equals("scissor")&&choice1.equals("rock")))
                 }
-                /* write results */
-                PrintWriter Writer1 = new PrintWriter(socket1.getOutputStream());
+                /* write results to clients*/
                 Writer1.println(ret1);
                 Writer1.flush();
-                Writer1.close();
-                buffer1.close();
-                PrintWriter Writer2 = new PrintWriter(socket2.getOutputStream());
                 Writer2.println(ret2);
                 Writer2.flush();
-                Writer2.close();
-                buffer2.close();
-                System.out.println("***Game Over");
+                /* read end request from clients */
+                String end1 = buffer1.readLine();
+                String end2 = buffer2.readLine();
+                if(end1.equals("end") || end2.equals("end")){
+                    Writer1.println("end");
+                    Writer1.flush();
+                    Writer2.println("end");
+                    Writer2.flush();
+                    break;
+                }else {
+                    Writer1.println("continue");
+                    Writer1.flush();
+                    Writer2.println("continue");
+                    Writer2.flush();
+                }
             }
+            System.out.println("***Game Over");
+            Writer1.close();
+            buffer1.close();
+            Writer2.close();
+            buffer2.close();
         } catch (Exception x) {
         x.printStackTrace();
         }
